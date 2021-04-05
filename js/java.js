5b73c14f6e061dfa3c738ec8b220d78c966e3fe3
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-04-05 16:10:12 +0300

    new

---
+++ js/java.js
@@ -0,0 +1,86 @@
+'use strict'
+
+let answer1 = prompt ('do you have any problem?');
+console.log(answer1);
+answer1 =answer1.toLocaleLowerCase();
+console.log(answer1);
+switch(answer1){
+
+    case 'yes':
+        alert('you have a problem');
+        break;
+    case'y':
+alert('you have a problem');
+break;
+    case 'no':
+        alert(' I have no problem ');
+        break;
+    case 'n' :
+ alert(' I have no problem ');
+ break ;
+
+}
+
+let answer2 = prompt ('Did you play football?');
+console.log(answer2);
+answer2 =answer2.toLocaleLowerCase();
+console.log(answer2);
+switch(answer2){
+
+    case 'yes':
+    case'y':
+alert('yes i did');
+break;
+    case 'no':
+    case 'n' :
+ alert(' no ');
+ break ;
+} 
+
+let answer3 = prompt ('Do you like apples?');
+console.log(answer3);
+answer3 =answer3.toLocaleLowerCase();
+console.log(answer3);
+switch(answer3){
+
+    case 'yes':
+    case'y':
+alert('yes i do');
+break;
+    case 'no':
+    case 'n' :
+ alert(' no ');
+ break ;
+} 
+
+
+let answer4 = prompt ('Do You have a laptop?');
+console.log(answer4);
+answer4 =answer4.toLocaleLowerCase();
+console.log(answer4);
+switch(answer4){
+
+    case 'yes':
+    case'y':
+alert('yes i have');
+break;
+    case 'no':
+    case 'n' :
+ alert(' no ');
+ break ;
+}
+let answer5 = prompt ('do you have a phone?');
+console.log(answer5);
+answer5 =answer5.toLocaleLowerCase();
+console.log(answer5);
+switch(answer5){
+
+    case 'yes':
+    case'y':
+alert('yes i have');
+break;
+    case 'no':
+    case 'n' :
+ alert(' no ');
+ break ;
+}
